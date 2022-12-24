35c050478ab8d7a2b85866bdf29eb45190e55c02
David Requeno <davidrequeno52@gmail.com>
2022-12-24 13:50:53 -0600

    David Requeno

---
+++ js/index.js
@@ -0,0 +1,11 @@
+new TypeIt("#title", {
+    strings: ["David Requeno"],
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
+
+  new TypeIt("#title-2", {
+    strings: ["Desarrollador web"],
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
